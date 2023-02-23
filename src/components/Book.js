5c979bdcf51cf6a7b2881d291432878c06722311
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-02-23 19:52:46 +0300

    fix linter errors

--- src/components/Book.js
+++ src/components/Book.js
@@ -7,10 +7,14 @@ function Book() {
         <li>
           <h2>Dune</h2>
           <span>Frank Herbert</span>
+          <br />
+          <button type="button">Delete</button>
         </li>
         <li>
           <h2>Hunger Games</h2>
           <span>Suzanne Collins</span>
+          <br />
+          <button type="button">Delete</button>
         </li>
       </ul>
     </div>
