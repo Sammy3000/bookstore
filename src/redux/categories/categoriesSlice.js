b01dc07a6b3d42123f8e646598fc7782ef9af6d3
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-03-01 08:20:58 +0300

    fix linter errors

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,12 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
+
 const initialState = [];
 const categoriesSlice = createSlice({
-  name: "category",
+  name: 'category',
   initialState,
   reducers: {
     checkStatus: (state) => {
       if (state === []) {
-        return "Under Construction";
+        return 'Under Construction';
       }
       return state;
     },
