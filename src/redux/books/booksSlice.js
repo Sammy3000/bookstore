da57afbb48e50274c78436c15ff5d4c7e7cc9542
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-03-01 11:23:11 +0300

    modify addbook action

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,12 +1,8 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
-const initialState = [
-  { id: '1', title: 'GOT', author: 'John Snow' },
-  { id: '2', title: 'Breaking Bad', author: 'Walter' },
-  { id: '3', title: 'Ozark', author: 'Matt' },
-];
+const initialState = [];
 const booksSlice = createSlice({
-  name: 'books',
+  name: "books",
   initialState,
   reducers: {
     addBook: (state, action) => {
@@ -15,9 +11,10 @@ const booksSlice = createSlice({
         title: action.payload.title,
         author: action.payload.author,
       };
-      state.concat(newBook);
+      state.push(newBook);
     },
-    deleteBook: (state, action) => state.filter((book) => book.id !== action.payload),
+    deleteBook: (state, action) =>
+      state.filter((book) => book.id !== action.payload),
   },
 });
 export const { addBook, deleteBook } = booksSlice.actions;
